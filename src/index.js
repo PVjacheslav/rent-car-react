bf7af572fc36b261e2eb6ccc197c391725196fc7
PVjacheslav <vjacheslav.pugachev@gmail.com>
2024-02-24 22:20:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 // import { PersistGate } from 'redux-persist/integration/react';
 
 import App from 'components/App/App';
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Provider> */}
-    {/* <BrowserRouter basename="rent-car-react"> */}
-    {/* <PersistGate loading={null}> */}
-    <App />
-    {/* </PersistGate> */}
-    {/* </BrowserRouter> */}
+    <BrowserRouter basename="rent-car-react">
+      {/* <PersistGate loading={null}> */}
+      <App />
+      {/* </PersistGate> */}
+    </BrowserRouter>
     {/* </Provider> */}
   </React.StrictMode>
 );
