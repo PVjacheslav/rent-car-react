e1fb771d7fa9dbbf045c75f5462d2e6a5cc8e193
PVjacheslav <vjacheslav.pugachev@gmail.com>
2024-02-24 20:00:51 +0200

    create routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
-import NavBar from 'components/NavBar/NavBar';
+import Layout from 'components/Layout/Layout';
 
-const Home = lazy(() => import('../../pages/home/home'));
-const Catalog = lazy(() => import('../../pages/catalog/catalog'));
-// const Favorites = lazy(() => import('../../pages/Favorites/Favorites'));
+const Home = lazy(() => import('../../pages/home/Home'));
+const Catalog = lazy(() => import('../../pages/catalog/Catalog'));
+const Favorites = lazy(() => import('../../pages/favorites/Favorites'));
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<NavBar />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/catalog" element={<Catalog />} />
-          {/* <Route path="/favorites" element={<Favorites />} /> */}
+          <Route path="/favorites" element={<Favorites />} />
           <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </Router>
+
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 import App from 'components/App/App';
 import './index.css';
@@ -10,12 +10,12 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <BrowserRouter basename="rent-car-react">
-        <PersistGate loading={null}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </Provider>
+    {/* <Provider> */}
+    <BrowserRouter basename="rent-car-react">
+      {/* <PersistGate loading={null}> */}
+      <App />
+      {/* </PersistGate> */}
+    </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>
 );
