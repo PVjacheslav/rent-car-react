91ed74498fee6a5a67e5089c7f0cd1eee8a9053a
PVjacheslav <vjacheslav.pugachev@gmail.com>
2024-02-24 22:22:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Provider> */}
-    <BrowserRouter basename="rent-car-react">
+    <BrowserRouter>
       {/* <PersistGate loading={null}> */}
       <App />
       {/* </PersistGate> */}
