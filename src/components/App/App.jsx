15de1497183b76e52b0dccef51ce78d5c15adce8
PVjacheslav <vjacheslav.pugachev@gmail.com>
2024-02-25 10:30:15 +0200

    try fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,21 +2,19 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from 'components/Layout/Layout';
 
-const Home = lazy(() => import('../../pages/Home/Home'));
+const Home = lazy(() => import('../../pages/home/Home'));
 const Catalog = lazy(() => import('../../pages/Catalog/Catalog'));
-const Favorites = lazy(() => import('../../pages/Favorites/Favorites'));
+
 
 export default function App() {
   return (
-    
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="*" element={<Home />} />
-        </Route>
-      </Routes>
-
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="catalog" element={<Catalog />} />
+        
+        <Route path="*" element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
--- src/pages/favorites/Favorites.jsx
+++
@@ -1,9 +0,0 @@
-import { Wrapper } from "./Favorites.styled";
-
-export default function Favorites() {
-  return (
-    <Wrapper>
-      <h2>Hello Favorites</h2>
-    </Wrapper>
-  );
-}
