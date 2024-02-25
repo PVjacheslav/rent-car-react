d11230def64148eec0a47b1ed7df1c13a9039548
PVjacheslav <vjacheslav.pugachev@gmail.com>
2024-02-25 06:01:12 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,9 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import Layout from 'components/Layout/Layout';
 
-const Home = lazy(() => import('../../pages/home/Home'));
-// const Catalog = lazy(() => import('../../pages/catalog/Catalog'));
-const Favorites = lazy(() => import('../../pages/favorites/Favorites'));
+const Home = lazy(() => import('pages/home/Home'));
+const Catalog = lazy(() => import('pages/catalog/Catalog'));
+const Favorites = lazy(() => import('pages/favorites/Favorites'));
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="/catalog" element={<Catalog />} /> */}
+          <Route path="/catalog" element={<Catalog />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="*" element={<Home />} />
         </Route>
